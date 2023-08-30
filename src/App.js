65901611b57907aa0a2132eb10baff5d3aa690ea
HaRsHiTsInGh23 <sharshit2306@gmail.com>
2023-08-30 23:15:55 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Header from "./Header";
+import NavBar from "./components/Navbar";
+import HeaderTop from "./HeaderTop";
+import MainBanner from "./MainBanner";
+import Footer from "./Footer";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <HeaderTop />
+      <Header />
+      <div className="nav-area">
+        <NavBar />
+      </div>
+      <MainBanner />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+export default App;
